ab8e6d71355e76d74c3d32de14f50c1268d64e16
Micke Lisinge <hi@micke.me>
2018-11-21 20:57:09 +0100

    Add devflow formula

---
+++ devflow.rb
@@ -0,0 +1,16 @@
+class Devflow < Formula
+  desc "Utility to create branches from Targetprocess stories"
+  homepage "https://github.com/micke/devflow"
+  url "https://github.com/micke/devflow/releases/download/v0.0.1/mac.tar.gz"
+  sha256 "c3a36931d586b6185dd79082d28f3223477a48848026bd2281ecf0506217858a"
+
+  bottle :unneeded
+
+  def install
+    bin.install "delta"
+  end
+
+  test do
+    system "#{bin}/devflow", "--version"
+  end
+end
