84861ccb4dc86c8b79f5ad9ee8cc97b7bdbaf3b3
Micke Lisinge <hi@micke.me>
2018-11-21 21:02:44 +0100

    Update sha

--- devflow.rb
+++ devflow.rb
@@ -2,7 +2,7 @@ class Devflow < Formula
   desc "Utility to create branches from Targetprocess stories"
   homepage "https://github.com/micke/devflow"
   url "https://github.com/micke/devflow/releases/download/v0.0.1/mac.tar.gz"
-  sha256 "c3a36931d586b6185dd79082d28f3223477a48848026bd2281ecf0506217858a"
+  sha256 "ae86beefb50a144914327803b5355ea642f554b2654fa5ec286a737926dad2bd"
 
   bottle :unneeded
 
