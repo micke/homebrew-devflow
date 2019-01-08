de0f97655267e2977e3c39b246ba1eac71a591f7
Micke Lisinge <hi@micke.me>
2019-01-08 16:07:36 +0100

    Version bump

--- devflow.rb
+++ devflow.rb
@@ -1,8 +1,8 @@
 class Devflow < Formula
   desc "Utility to create branches from Targetprocess stories"
   homepage "https://github.com/micke/devflow"
-  url "https://github.com/micke/devflow/releases/download/v0.0.5/mac.tar.gz"
-  sha256 "7d7a331ea626ed42f728c1562ab33bcf23599d68f4baf2b63b3f850d4edb28b3"
+  url "https://github.com/micke/devflow/releases/download/v0.0.6/mac.tar.gz"
+  sha256 "c09f3182a011d30ce78d8240b9bd108cc0146a36591a940fc2d96395285376b5"
 
   bottle :unneeded
 
